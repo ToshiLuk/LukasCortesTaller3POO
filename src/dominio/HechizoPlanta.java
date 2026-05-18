ea8dd3325e8d878891cb121e607a4f95dbe123b6
Lukas Cortes <mrvro@NotebookToshi>
2026-05-18 17:58:39 -0400

    Clase Hechizo Planta

---
+++ src/dominio/HechizoPlanta.java
@@ -0,0 +1,17 @@
+package dominio;
+
+public class HechizoPlanta extends Hechizo{
+	private int duracionStun;
+	private int cantPlanta;
+	public HechizoPlanta(String nombre, String tipo, int daño, int duracionStun, int cantPlanta) {
+		super(nombre, tipo, daño);
+		this.duracionStun = duracionStun;
+		this.cantPlanta = cantPlanta;
+	}
+
+	@Override
+	public double calcularPuntuacion() {
+		return this.daño + (this.duracionStun * this.cantPlanta);
+	}
+	
+}
