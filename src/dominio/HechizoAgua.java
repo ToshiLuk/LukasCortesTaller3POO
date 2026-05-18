0eb82efa2c66cf0b7dab3ad7c044389a89ada87f
Lukas Cortes <mrvro@NotebookToshi>
2026-05-18 18:02:06 -0400

    Clase Hechizo Agua

---
+++ src/dominio/HechizoAgua.java
@@ -0,0 +1,16 @@
+package dominio;
+
+public class HechizoAgua extends Hechizo{
+	private int cantHeal;
+	private int presionAgua;
+	public HechizoAgua(String nombre, String tipo, int daño, int cantHeal, int presionAgua) {
+		super(nombre, tipo, daño);
+		this.cantHeal = cantHeal;
+		this.presionAgua = presionAgua;
+	}
+	@Override
+	public double calcularPuntuacion() {
+		return (this.daño + this.cantHeal + this.presionAgua)*2;
+	}
+	
+}
