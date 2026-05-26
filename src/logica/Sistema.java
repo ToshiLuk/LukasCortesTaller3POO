cc858f6d821f91d5fe20b78208d094b8c73542ef
Lukas Cortés <lukas.cortes01@alumnos.ucn.cl>
2026-05-26 12:10:47 -0400

    Getters y metodo modificarMago en la interfaz Sistema

--- src/logica/Sistema.java
+++ src/logica/Sistema.java
@@ -1,10 +1,16 @@
 package logica;
 
+import java.util.ArrayList;
+
 import dominio.Hechizo;
+import dominio.Mago;
 
 public interface Sistema {
 	void guardarHechizos();
 	void guardarMagos();
 	Hechizo buscarHechizo(String nombreHechizo);
 	boolean agregarMago(String nombreMago);
+	ArrayList<Mago> getMagos();
+	ArrayList<Hechizo> getHechizos();
+	boolean modificarMago(String nombreMagoMod);
 }
